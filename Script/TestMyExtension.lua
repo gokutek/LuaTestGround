f1bb50401d25c112c46ca89409e67ab8c43cd810
gokutek <vxer@qq.com>
2020-09-25 14:07:20 +0800

    测试：Lua扩展库

--- Script/TestMyExtension.lua
+++ Script/TestMyExtension.lua
@@ -2,3 +2,6 @@ local MyExtension = require("MyExtension")
 local n, s = MyExtension.encode()
 print(n)
 print(s)
+
+local MyExtensionMath = require("MyExtension.Math")
+print(MyExtensionMath.add(10, 20))

9242ec7d7736997e8fe6bd48ae94dad5b362568d
gokutek <vxer@qq.com>
2021-01-28 17:19:29 +0800

    测试：lua_pcall的错误处理函数

--- Source/Test/test_capi.cpp
+++ Source/Test/test_capi.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <assert.h>
 #include <string.h>
 #include "lua.hpp"
@@ -262,6 +263,42 @@ static void test_global_c_lua()
 	lua_close(L);
 }
 
+static int error_func(lua_State* L)
+{
+	int Type = lua_type(L, -1);
+	if (Type == LUA_TSTRING)
+	{
+		const char* ErrorString = lua_tostring(L, -1);
+		luaL_traceback(L, L, ErrorString, 1);
+		ErrorString = lua_tostring(L, -1);
+		std::cerr << "Lua error message: " << ErrorString << std::endl;
+	}
+	else if (Type == LUA_TTABLE)
+	{
+		// multiple errors is possible
+		int MessageIndex = 0;
+		lua_pushnil(L);
+		while (lua_next(L, -2) != 0)
+		{
+			const char* ErrorString = lua_tostring(L, -1);
+			std::cerr << "Lua error message: " << MessageIndex++ << " : " << ErrorString << std::endl;
+			lua_pop(L, 1);
+		}
+	}
+
+	lua_pop(L, 1);
+	return 0;
+}
+
+static void test_pcall()
+{
+	lua_State* L = luaL_newstate();
+	lua_pushcfunction(L, error_func);
+	luaL_loadfile(L, "../Script/test_capi_pcall.lua");	// [-0, +1, m]
+	lua_pcall(L, 0, LUA_MULTRET, -2);
+	lua_close(L);
+}
+
 void api_test_main()
 {
 	test_lua_type();
@@ -277,6 +314,7 @@ void api_test_main()
 	test_call_require();
 	test_newmetatable();
 	test_global_c_lua();
+	test_pcall();
 }
 
 /*
